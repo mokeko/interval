375744bf9e9f3325747fcc3d583c945f36065203
mokeko <Anthurium.1605@gmail.com>
2023-05-01 17:37:00 +0900

    space

--- README.md
+++ README.md
@@ -12,31 +12,37 @@ Determine if two Intervals overlap.
 
 ```go
 type Int = interval.Int
+
 // [1, 3]
 i := interval.New(
   interval.ClosedEp(Int(1)),
   interval.ClosedEp(Int(3)),
 )
+
 // (3, +inf)
 i2 := interval.New(
   interval.OpenEp(Int(3)),
   interval.UnboundedEp[Int](),
 )
+
 fmt.Println(i.Overlap(i2)) // false
 ```
 - Time
 ```go
 type Time = interval.Time
+
 // [2020-01-01, 2020-01-04]
 i := interval.New(
   interval.ClosedEp(Time(time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC))),
   interval.ClosedEp(Time(time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC))),
 )
+
 // [2020-01-02, 2020-01-03]
 i2 := interval.New(
   interval.ClosedEp(Time(time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC))),
   interval.ClosedEp(Time(time.Date(2020, 1, 3, 0, 0, 0, 0, time.UTC))),
 )
+
 fmt.Println(i.Overlap(i2)) // true
 ```
 - Other types
@@ -61,10 +67,12 @@ i := interval.New(
   interval.ClosedEp(ver{1, 0}),
   interval.OpenEp(ver{2, 0}),
 )
+
 // [1.5, 2.5)
 i2 := interval.New(
   interval.ClosedEp(ver{1, 5}),
   interval.OpenEp(ver{2, 5}),
 )
+
 fmt.Println(i.Overlap(i2)) // true
 ```
--- example_test.go
+++ example_test.go
@@ -11,31 +11,37 @@ func Example() {
 	// Int
 	{
 		type Int = interval.Int
+
 		// [1, 3]
 		i := interval.New(
 			interval.ClosedEp(Int(1)),
 			interval.ClosedEp(Int(3)),
 		)
+
 		// (3, +inf)
 		i2 := interval.New(
 			interval.OpenEp(Int(3)),
 			interval.UnboundedEp[Int](),
 		)
+
 		fmt.Println(i.Overlap(i2)) // false
 	}
 	// Time
 	{
 		type Time = interval.Time
+
 		// [2020-01-01, 2020-01-04]
 		i := interval.New(
 			interval.ClosedEp(Time(time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC))),
 			interval.ClosedEp(Time(time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC))),
 		)
+
 		// [2020-01-02, 2020-01-03]
 		i2 := interval.New(
 			interval.ClosedEp(Time(time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC))),
 			interval.ClosedEp(Time(time.Date(2020, 1, 3, 0, 0, 0, 0, time.UTC))),
 		)
+
 		fmt.Println(i.Overlap(i2)) // true
 	}
 	// Custom Type
@@ -55,16 +61,19 @@ func Example() {
 				return v.major < v2.major || (v.major == v2.major && v.minor < v2.minor)
 			}
 		*/
+
 		// [1.0, 2.0)
 		i := interval.New(
 			interval.ClosedEp(ver{1, 0}),
 			interval.OpenEp(ver{2, 0}),
 		)
+
 		// [1.5, 2.5)
 		i2 := interval.New(
 			interval.ClosedEp(ver{1, 5}),
 			interval.OpenEp(ver{2, 5}),
 		)
+
 		fmt.Println(i.Overlap(i2)) // true
 	}
 	// Output:
